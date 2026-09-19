6c7cded1225bd9d02d8d0578c50a6b8f7234ea04
locopy0121-hub <locopy0121@gmail.com>
2026-09-19 21:28:01 +0800

    chore(version): align runtime constants V5.0.5 [skip ci]

--- src/v3/version.ts
+++ src/v3/version.ts
@@ -1,4 +1,4 @@
-export const APP_SEMVER='5.0.4';
-export const APP_BUILD=51;
+export const APP_SEMVER='5.0.5';
+export const APP_BUILD=52;
 export const APP_DISPLAY_VERSION=`V${APP_SEMVER}`;
 export const APP_VERSION_LABEL=APP_DISPLAY_VERSION;
