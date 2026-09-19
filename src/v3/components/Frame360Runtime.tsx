1599f8d5898ec40b3a7b7893e04ff4fecef3e6cf
locopy0121-hub <locopy0121@gmail.com>
2026-09-19 21:00:25 +0800

    fix(360): use React Native absolute fill style

--- src/v3/components/Frame360Runtime.tsx
+++ src/v3/components/Frame360Runtime.tsx
@@ -432,7 +432,7 @@ function RuntimeCellSurface({
             <View
               pointerEvents="none"
               style={[
-                StyleSheet.absoluteFillObject,
+                StyleSheet.absoluteFill,
                 {
                   backgroundColor: cell.style.backgroundOverlayColor,
                   opacity: (cell.style.backgroundOverlayOpacity ?? 0) / 100,
