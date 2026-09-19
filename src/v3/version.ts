ec2d11923a5c0b122665060b0110a0914cb61232
locopy0121-hub <locopy0121@gmail.com>
2026-09-19 18:33:35 +0800

    chore(release): bump app version constants to V5.0.3

--- src/v3/version.ts
+++ src/v3/version.ts
@@ -1,4 +1,4 @@
-export const APP_SEMVER='5.0.2';
-export const APP_BUILD=49;
+export const APP_SEMVER='5.0.3';
+export const APP_BUILD=50;
 export const APP_DISPLAY_VERSION=`V${APP_SEMVER}`;
 export const APP_VERSION_LABEL=APP_DISPLAY_VERSION;
